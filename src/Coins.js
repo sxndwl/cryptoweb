03bd558a4293d83c6d4fad865f99ad407d5aaf26
sxndwi <s.fomchenka@my18.ru>
2023-02-12 19:15:50 +0300

    redux((

--- src/Coins.js
+++ src/Coins.js
@@ -5,24 +5,53 @@ import UseBinance from './api/usewebsocket'
 const Coins = () => {
     const valutes = [
         'btcusdt@miniTicker@1000ms',
-        //         'ethusdt@miniTicker@1000ms'
+        'ltcusdt@miniTicker@1000ms',
+        'dogeusdt@miniTicker@1000ms',
+        'ethusdt@miniTicker@1000ms',
+        'dashusdt@miniTicker@1000ms',
+        'xrpusdt@miniTicker@1000ms',
     ]
     const { coursesInfo, isPriceGoingUp } = UseBinance(valutes);
+    console.log(coursesInfo)
     return (
-        <Wrapper>
+        <Wrapper width='360px'>
             <Title size={18} weight={700}>Coins</Title>
             <Currency
-                name='BTC'
-                subname='Bitcoin'
-                currencyValue={coursesInfo}
-                condition={isPriceGoingUp}
+            name='BTC'
+            subname='Bitcoin'
+            currencyValue={coursesInfo}
+            condition={isPriceGoingUp}
             />
-            {/* <Currency
-            name='BTC2'
-            subname='Bitcoin2'
-            currencyValue={coursesInfo2}
-            condition={isPriceGoingUp2}
-            /> */}
+            <Currency
+            name='LTC'
+            subname='Litecoin'
+            currencyValue={coursesInfo}
+            condition={isPriceGoingUp}
+            />
+            <Currency
+            name='XDG'
+            subname='Dogecoin'
+            currencyValue={coursesInfo}
+            condition={isPriceGoingUp}
+            />
+            <Currency
+            name='ETH'
+            subname='Ethereum'
+            currencyValue={coursesInfo}
+            condition={isPriceGoingUp}
+            />
+            <Currency
+            name='DASH'
+            subname='Dash'
+            currencyValue={coursesInfo}
+            condition={isPriceGoingUp}
+            />
+            <Currency
+            name='XRP'
+            subname='Ripple'
+            currencyValue={coursesInfo}
+            condition={isPriceGoingUp}
+            />                                    
         </Wrapper>
     )
 }
